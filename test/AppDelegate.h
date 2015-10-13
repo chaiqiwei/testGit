89e0febd19c32d3a887f4d66f87e82ca39ba8605
chaiqiwei <535568752@qq.com>
2015-10-13 10:42:30 +0800

    第一次提交

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by pengsi_2015 on 15/10/13.
+//  Copyright (c) 2015年 pengsi.test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by pengsi_2015 on 15/10/13.
+//  Copyright (c) 2015年 pengsi.test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
